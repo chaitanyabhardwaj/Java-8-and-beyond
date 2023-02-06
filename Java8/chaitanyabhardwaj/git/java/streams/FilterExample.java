a747d7b3ffb69176994a99e87f9179827dbb6b0a
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-02-06 20:02:19 +0530

    Java8 - Filter

---
+++ Java8/chaitanyabhardwaj/git/java/streams/FilterExample.java
@@ -0,0 +1,29 @@
+package chaitanyabhardwaj.git.java.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chaitanyabhardwaj.git.java.ConsumerExample1.Guitar;
+import chaitanyabhardwaj.git.java.ConsumerExample1.GuitarStore;
+
+public class FilterExample {
+    
+    public static void main(String args[]) {
+        //initializing guitar
+        Guitar g1 = new Guitar("Epiphone", 500);
+        Guitar g2 = new Guitar("Fender", 600);
+        Guitar g3 = new Guitar("Gibson", 700);
+
+        //initializing guitarStore
+        List<Guitar> list = new ArrayList<>();
+        list.add(g1);
+        list.add(g2);
+        list.add(g3);
+        GuitarStore gStore = new GuitarStore("Venus",list);
+
+        //using filter method to filter out guitars with price lower than 650
+        gStore.geGuitars().stream()
+        .filter(g -> g.getPrice() < 650)
+        .forEach(System.out::println);
+    }
+}
