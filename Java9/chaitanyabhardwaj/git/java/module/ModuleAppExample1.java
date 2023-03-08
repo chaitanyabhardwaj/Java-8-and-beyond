32734672145dda1bbb1b44127d03593c14debe58
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-03-09 02:03:48 +0530

    Java9 - Module Example - Experimental(Will a module run multiple files in the specified package name which matches the module name?)

---
+++ Java9/chaitanyabhardwaj/git/java/module/ModuleAppExample1.java
@@ -0,0 +1,24 @@
+package chaitanyabhardwaj.git.java.module;
+
+import java.util.List;
+
+import chaitanyabhardwaj.git.java.GuitarStore;
+import chaitanyabhardwaj.git.java.GuitarStore.Guitar;
+
+public class ModuleAppExample1 {
+    
+    public static void main(String[] args) {
+        Guitar g1 = new Guitar("Epiphone", 500f);
+        Guitar g2 = new Guitar("Fender", 600f);
+        Guitar g3 = new Guitar("Gibson", 700f);
+
+        //List.of is a static factory method introduced in java 9
+        //Along with many others.
+        final List<Guitar> guitarList = List.of(g1,g2,g3);
+
+        GuitarStore gStore = new GuitarStore("Venus", guitarList);
+        System.out.println("Class name " + ModuleAppExample1.class.getName());
+        gStore.getGuitarList().forEach(System.out::println);
+    }
+
+}
