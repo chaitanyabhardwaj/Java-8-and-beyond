cd9cc19d8fe1a81e8d3d6c78dd2fa010193180ad
Chaitanya Bhardwaj <chaitanyabhardwaj89@gmail.com>
2023-05-13 02:33:15 +0530

    Java13 - String API - Text blocks

---
+++ Java13/src/TextBlocksExample.java
@@ -0,0 +1,12 @@
+public class TextBlocksExample {
+    public static void main(String[] args) throws Exception {
+        var multiline = """
+                There once was a ship that put to sea
+                The name of the ship was the Billy O' Tea
+                The winds blew hard dipped down her bow
+                Blow my bully boys blow
+                Ha!
+                """;
+        System.out.println(multiline);
+    }
+}
